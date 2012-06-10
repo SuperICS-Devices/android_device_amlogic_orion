10a1445562fa514ff92a75dd530db6f998c711fb
Alexandre Dumont <adumont@gmail.com>
2012-06-10 13:59:08 +0200

    Initial commit NVSBL Orion
    Massive Kang of NVSBL Sirius device files by CML

---
+++ device.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_PREBUILT_KERNEL=device/amlogic/orion/kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/amlogic/orion/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+# Inherit generic amlogic device
+$(call inherit-product, device/amlogic/aml-common/aml-common.mk)
+
+
+# Modules
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel \
+	device/amlogic/orion/prebuilt/modules/mali.ko:system/lib/modules/mali.ko \
+	device/amlogic/orion/prebuilt/modules/ump.ko:system/lib/modules/ump.ko \
+	device/amlogic/orion/prebuilt/modules/cifs.ko:system/lib/modules/cifs.ko \
+	device/amlogic/orion/prebuilt/modules/tun.ko:system/lib/modules/tun.ko \
+	device/amlogic/orion/prebuilt/modules/ext4.ko:system/lib/modules/ext4.ko
+
+# Sensors
+PRODUCT_COPY_FILES += \
+	device/amlogic/orion/prebuilt/sensors/sensors.amlogic.so:system/lib/hw/sensors.amlogic.so \
+	device/amlogic/orion/prebuilt/sensors/memsicd:system/bin/memsicd
+
+# Input device calibration files
+PRODUCT_COPY_FILES += \
+	device/amlogic/orion/prebuilt/Goodix_TouchScreen_of_Guitar.idc:system/usr/idc/Goodix_TouchScreen_of_Guitar.idc
+
+# Display modes
+PRODUCT_COPY_FILES += \
+	device/amlogic/orion/prebuilt/set_display_mode.sh:system/bin/set_display_mode.sh \
+
+# Gsensor
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.gsensorposition=3 \
+	sys.fb.bits=32 \
+	ro.setupwizard.mode=OPTIONAL
---
+++ osr_orion.mk
@@ -0,0 +1,38 @@
+# Release name
+PRODUCT_RELEASE_NAME := Orion
+
+# Inherit device configuration
+$(call inherit-product, vendor/osr/products/asian.mk)
+
+$(call inherit-product, vendor/osr/products/hdpi.mk)
+
+$(call inherit-product, vendor/osr/products/high_tablet_device.mk)
+
+$(call inherit-product, device/amlogic/orion/full_orion.mk)
+
+#test in ICS-RC
+PRODUCT_PACKAGES += \
+    ROMControl
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := osr_orion
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := orion
+PRODUCT_MODEL := P4D Orion
+PRODUCT_MANUFACTURER := Amlogic
+PRODUCT_VERSION_MAINTENANCE := 1
+
+PRODUCT_CHARACTERISTICS := tablet
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=aml8726m BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.4/IMM76/292727:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.4 IMM76 292727 release-keys"
+
+PRODUCT_PACKAGE_OVERLAYS += device/amlogic/orion/overlay
+
+# Inherit Gapps blobs
+-include vendor/osr/products/minimal_gapps.mk
+
+# Inherit Flash Player
+-include vendor/osr/products/flashplayer.mk
+
+PRODUCT_LOCALES := es_ES ca_ES zh_CN ja_JP ko_KR ru_RU en_US en_GB fr_FR it_IT de_DE hdpi

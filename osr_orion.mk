8987d8af4b78663393f4ae12d8df2f77f612a1f7
cmartinezlozano <chaco_Sc_21@hotmail.com>
2012-07-02 20:26:17 +0200

    Change hdpi to mdpi because resolution

--- osr_orion.mk
+++ osr_orion.mk
@@ -4,7 +4,7 @@ PRODUCT_RELEASE_NAME := Orion
 # Inherit device configuration
 $(call inherit-product, vendor/osr/products/asian.mk)
 
-$(call inherit-product, vendor/osr/products/hdpi.mk)
+$(call inherit-product, vendor/osr/products/mdpi.mk)
 
 $(call inherit-product, vendor/osr/products/high_tablet_device.mk)
 
@@ -20,7 +20,7 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := orion
 PRODUCT_MODEL := P4D Orion
 PRODUCT_MANUFACTURER := Amlogic
-PRODUCT_VERSION_MAINTENANCE := 1
+PRODUCT_VERSION_MAINTENANCE := 0
 
 PRODUCT_CHARACTERISTICS := tablet
 
@@ -35,4 +35,4 @@ PRODUCT_PACKAGE_OVERLAYS += device/amlogic/orion/overlay
 # Inherit Flash Player
 -include vendor/osr/products/flashplayer.mk
 
-PRODUCT_LOCALES := es_ES ca_ES zh_CN ja_JP ko_KR ru_RU en_US en_GB fr_FR it_IT de_DE hdpi
+PRODUCT_LOCALES := es_ES ca_ES zh_CN ja_JP ko_KR ru_RU en_US en_GB fr_FR it_IT de_DE mdpi
